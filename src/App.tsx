e6ad532503cf1a87adb6ac55f4876118571825a4
Érico Marshall <ericogmarshall1@gmail.com>
2024-08-31 21:25:23 -0300

    [FEAT] Adds sidebar and styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,76 @@
+import twitterLogo from "./assets/logo-twitter.svg";
+import {
+  Bell,
+  BookmarkSimple,
+  DotsThreeCircle,
+  Envelope,
+  FileText,
+  Hash,
+  House,
+  Sparkle,
+  User,
+} from "phosphor-react";
+
 export function App() {
   return (
-    <>
-      <h1>Hello, world!</h1>
-      <p>Welcome to your new single-spa application.</p>
-    </>
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="logo" />
+        <nav className="main-navigation">
+          <a className="active" href="">
+            <House weight="fill" />
+            Home
+          </a>
+          <a href="">
+            <Hash weight="fill" />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button className="new-tweet">Tweet</button>
+      </aside>
+      <div className="content">
+        <main className="timeline">
+          <header className="timeline-header">
+            Home
+            <Sparkle />
+          </header>
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/egmarshall.png" alt="user photo" />
+
+              <textarea id="tweet" placeholder="What's happening?" />
+            </label>
+
+            <button type="submit">Tweet</button>
+          </form>
+        </main>
+      </div>
+    </div>
   );
 }
